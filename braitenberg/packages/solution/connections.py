fd27263906282f4dfebebb68c4046c2d63e54726
Dino Claro <clrdin001@myuct.ac.za>
2023-06-27 12:14:53 +0200

    First Submission 1

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -7,8 +7,7 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
+    res[:, :319] = 1
     # ---
     return res
 
@@ -17,6 +16,6 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:300] = -1
+    res[:, 320:] = 1
     # ---
     return res
